47de69b3f0c16ba7999e25f620b640150a3ca822
LinHai <kami@cancanyou.com>
2020-09-08 13:00:29 +0800

    发布新版本。

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 module.exports = require('./lib');
+module.exports.CKLFEApi = require('./lib/CKLFEApi')
+module.exports.CKLFEDataErr = require('./lib/CKLFEDataErr')
+module.exports.CKLFEDataSuccess = require('./lib/CKLFEDataSuccess')
+module.exports.CKLRandText = require('./lib/CKLRandText')
